6590a1b53f635e38d0ecec00b60bc2c35b72d8ba
幹人 永澤 <230413@jc-21.jp>
2025-03-11 12:06:25 +0900

    a

---
+++ RedEnemySpawner.cpp
@@ -0,0 +1,15 @@
+//
+//#include "RedEnemySpawner.h"
+//
+//// コンストラクタ
+//RedEnemySpawner::RedEnemySpawner(GameObject* parent, int spawnLimit)
+//    : Spawner(parent, spawnLimit) {}
+//
+//// RedEnemy を生成
+//RedEnemy* RedEnemySpawner::CreateEnemy(const XMFLOAT3& spawnPos, const XMFLOAT3& moveDirection)
+//{
+//    RedEnemy* enemy = new RedEnemy(this);
+//    enemy->SetPos(spawnPos);
+//    enemy->SetMoveDirection(moveDirection);
+//    return enemy;
+//}
---
+++ RedEnemySpawner.h
@@ -0,0 +1,12 @@
+//#pragma once
+//#include "Spawner.h"
+//#include "RedEnemy.h"
+//#include"Engine/GameObject.h"
+//
+//class RedEnemySpawner : public Spawner<RedEnemy>
+//{
+//public:
+//    RedEnemySpawner(GameObject* parent, int spawnLimit = 10);
+//protected:
+//    RedEnemy* CreateEnemy(const XMFLOAT3& spawnPos, const XMFLOAT3& moveDirection) override;
+//};
