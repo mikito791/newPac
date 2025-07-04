fe0b88adc95138e4dd36371de771bdc523c55847
幹人 永澤 <230413@jc-21.jp>
2025-07-04 17:48:10 +0900

    あああ

--- Player.cpp
+++ Player.cpp
@@ -223,9 +223,6 @@ void Player::OnCollision(GameObject* pTarget)
 		if (pShield)
 		{
 			// 反転方向に対応した角度と位置をセット
-			int rot = pReversalBall->GetRotationFromReveralDirection(reversalDirection);
-			XMFLOAT3 pos = pReversalBall->GetPositionFromReveralDirection(reversalDirection);
-			pShield->SetTransform(pos, rot);
 		}
 		onReversal = true;
 		reversalTimer = 0.0f;
